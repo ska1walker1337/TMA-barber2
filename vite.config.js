95a57671a652ca70fed68a33717c849ec60537c7
vova <zsgurskij.vlsdimir@gmail.com>
2026-09-17 21:02:46 +0300

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/TMA-barber2/',
   server: {
     host: "0.0.0.0",
     port: 3000,
